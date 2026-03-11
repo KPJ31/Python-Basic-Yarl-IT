dd9c1c0112c36a3e278f9746be42af048ee37c04
kpj31 <kpjana31@gmail.com>
2026-03-11 08:56:36 +0530

    python files

---
+++ First.py
@@ -0,0 +1,13 @@
+#print("Welcome to Python")
+
+name="jana"
+age="23"
+
+#print(type(name))
+#print(type(age))
+
+#print("my name is: " + name)
+#print("my age is: " + age)
+
+print("my name is: ", name)
+print("my age is: ", age)
---
+++ MyInfo.py
@@ -0,0 +1,14 @@
+print("******************")
+print("My Information")
+print("******************")
+fname = "Janarthan"
+lname = "Panchalingam"
+age = 23
+dob = "31/01/2023"
+hight = 160
+print("First Name:\t"+fname)
+print("Last Name:\t"+lname)
+print("First Name:\t"+str(age))
+print("First Name:\t"+dob)
+print("First Name:\t"+str(hight))
+print("******************")
