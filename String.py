d4cfb557c5ad7cc3c1e1a86ccc30b0d586e7d929
kpj31 <kpjana31@gmail.com>
2026-03-12 20:06:16 +0530

    today update

---
+++ String.py
@@ -0,0 +1,12 @@
+"""
+String Format
+"""
+fname = "Janarthan"
+lname = "Panchalingam"
+'''
+#fullname= fname+" "+lname
+fullname = f"{fname} {lname}"
+
+print(f"Full name: {fullname}")
+print("Full Name:\t"+fullname)
+'''
---
+++ input.py
@@ -0,0 +1,54 @@
+'''
+name = input("Enter Your Name: ")
+
+print(type(name))
+print("Your name is: " + name)
+
+
+num1 = input("Enter Number1: ")
+num2 = input("Enter Number2: ")
+
+sum = int(num1) + int(num2)
+
+print(sum)
+
+
+fname = input("Enter first name: ")
+lname = input("Enter lastname: ")
+
+print("Full Name:\t"+ fname + " " + lname)
+print("Full Name %s"%fname)
+print("Full Name {}".format(fname))
+
+'''
+
+num = 10.8691
+
+
+'''
+print("Number %d"%num)
+print("Number %s"%num)
+print("Number %f"%num)
+print("Number %.2f"%num)
+print("Number %.3f%%"%num)
+
+
+print(f"This number {num}")
+print(f"This number {num:.2f}")
+print(f"This number {num+5}")
+print(f"This number {num+5:.2f}")
+print(f"This number {num:.1f}%")
+print(f"This number {num:.1f}cm")
+
+'''
+
+x=10
+y=5
+print(f"{x=}")
+print(f"{y=}")
+print(f"{x+y=}")
+print(f"{x-y=}")
+print(f"{x*y=}")
+print(f"{x/y=}")
+#print(f"{x+y=x+y}")
+print(f"{num=}")
