cea593463de2796048cb6851f983a0c19245e720
kpj31 <kpjana31@gmail.com>
2026-03-13 18:44:34 +0530

    hw update

---
+++ EX.py
@@ -0,0 +1,40 @@
+x=10
+y=5
+
+print(f"{x=}")
+print("x="+str(x))
+#print("x=",x)
+print("x=%d"%x)
+print("x={}".format(x))
+
+print ("*************************")
+
+print(f"{y=}")
+print(f"{x+y=}")
+print(f"{x-y=}")
+print(f"{x*y=}")
+print(f"{x/y=}")
+
+print ("*************************")
+
+print("y="+str(y)) #5
+print("x+y="+str(x+y))#15
+print("x-y="+str(x-y))#5
+print("x*y="+str(x*y))#50
+print("x/y="+str(x/y))#2
+
+print ("*************************")
+
+print("y=%d"%y)
+print("x+y=%d"%(x+y))
+print("x-y=%d"%(x-y))
+print("x*y=%d"%(x*y))
+print("x/y=%d"%(x/y))
+
+print ("*************************")
+
+print("y={}".format(y))
+print("x+y={}".format(x+y))
+print("x-y={}".format(x-y))
+print("x*y={}".format(x*y))
+print("x/y={}".format(x/y))
